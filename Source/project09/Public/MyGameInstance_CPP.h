04319a1ca17111633d2dff5be80f04b6ee93ace3
tongkangyu <1937074529@qq.com>
2026-05-09 19:14:15 +0800

    通过了c++编译

---
+++ Source/project09/Public/MyGameInstance_CPP.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MyGameInstance_CPP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT09_API UMyGameInstance_CPP : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
